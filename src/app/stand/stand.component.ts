33f783ce38b055e13c8fe514ccc38bbc4aefbe6a
abhaysps <67944135+abhaysps@users.noreply.github.com>
2020-07-08 19:53:50 +0530

    Add files via upload

---
+++ src/app/stand/stand.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stand',
+  templateUrl: './stand.component.html',
+  styleUrls: ['./stand.component.css']  
+})
+export class StandComponent implements OnInit {
+
+  constructor() {}
+  
+  ngOnInit() {}
+}
